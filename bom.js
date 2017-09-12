1debae347e92fcb1b7d5b3f94348f1425a7491c7
yangtaeho <yangbuk84@gmail.com>
2017-09-12 11:35:02 +0900

    Update bom.js

--- bom.js
+++ bom.js
@@ -1,24 +1,24 @@
 
-			function getCurrUrl() {
-				var tempurl = location.href.replace(location.pathname+location.search,'');
-				return tempurl;
-			}
-			function replaceHistory(url,pathname,queryStr) {
-				var tempUrl = '';
-				if(!url) {
-				    if (window.console) console.log('Invalid url.');
-				    return false;
-				}
-				tempUrl += url;
-				pathname && (tempUrl += pathname);
-				queryStr && (tempUrl += "?"+queryStr);
-				history.replaceState("", "", tempUrl);
-			}
-			function replacePath(pathname) {
-				replaceHistory(getCurrUrl(),pathname);
-			}
+function getCurrUrl() {
+	var tempurl = location.href.replace(location.pathname+location.search,'');
+	return tempurl;
+}
+function replaceHistory(url,pathname,queryStr) {
+	var tempUrl = '';
+	if(!url) {
+	if (window.console) console.log('Invalid url.');
+		return false;
+	}
+	tempUrl += url;
+	pathname && (tempUrl += pathname);
+	queryStr && (tempUrl += "?"+queryStr);
+	history.replaceState("", "", tempUrl);
+}
+function replacePath(pathname) {
+	replaceHistory(getCurrUrl(),pathname);
+}
       
-			/*
-       * for example
-       */
-			//replacePath('you want path');
+/**
+ * for example
+ */
+//replacePath('/you/want/path.do');
